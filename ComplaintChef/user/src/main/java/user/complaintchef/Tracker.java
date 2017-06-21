4b316093b28bdc9a9d3187fbaa3d45c026f9adb1
Natesh Relhan <nateshrelhan@gmail.com>
2017-06-21 16:15:20 +0530

    intermediate

--- ComplaintChef/user/src/main/java/user/complaintchef/Tracker.java
+++ ComplaintChef/user/src/main/java/user/complaintchef/Tracker.java
@@ -21,6 +21,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.json.JSONObject;
 
@@ -29,21 +31,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import common.complaintcheflib.model.User;
 import common.complaintcheflib.util.BaseAppCompatActivity;
 import common.complaintcheflib.util.ThreadExecutor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import user.complaintchef.core.MyApplication;
+import user.complaintchef.firebase.FirebaseDataStoreFactory;
 import user.complaintchef.net.APIService;
 
 /**
  * Created by nateshrelhan on 6/21/17.
  */
 
-public class Tracker extends BaseAppCompatActivity implements OnMapReadyCallback {
+public class Tracker extends BaseAppCompatActivity implements OnMapReadyCallback, FirebaseDataStoreFactory.ChildCallBack {
 
+    private static final String KEY_USER = "users";
     private static final long TRIGGER_DELAY_IN_MS = 1000;
+    private static DatabaseReference mDatabaseReference;
     private static int TRIGGER_FETCH = 101;
     private SupportMapFragment mapFragment;
     private ThreadExecutor threadExecutor;
@@ -51,6 +57,7 @@ public class Tracker extends BaseAppCompatActivity implements OnMapReadyCallback
     private APIService apiService;
     private WeakRefHandler weakRefHandler;
     private Bundle gotBundle;
+    private FirebaseDataStoreFactory<User> firebaseDataStoreFactory;
 
 
     public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
@@ -76,6 +83,8 @@ public class Tracker extends BaseAppCompatActivity implements OnMapReadyCallback
         apiService = MyApplication.getAPIService();
         weakRefHandler = new WeakRefHandler(this);
         gotBundle = getIntent().getExtras();
+        firebaseDataStoreFactory = new FirebaseDataStoreFactory<>();
+        firebaseDataStoreFactory.data(User.class, );
         mapFragment.getMapAsync(this);
     }
 
@@ -120,6 +129,39 @@ public class Tracker extends BaseAppCompatActivity implements OnMapReadyCallback
 
             }
         });
+    }
+
+    @Override
+    public void onChildAdded(Object child) {
+
+    }
+
+    @Override
+    public void onChildChanged(Object child) {
+
+    }
+
+    @Override
+    public void onChildRemoved(Object child) {
+
+    }
+
+    @Override
+    public void onChildMoved(Object child) {
+
+    }
+
+    @Override
+    public void onCancelled() {
+
+    }
+
+    private DatabaseReference getmDatabaseReference(String id) {
+        if (mDatabaseReference == null) {
+            mDatabaseReference = FirebaseDatabase.getInstance().getReference().child(KEY_USER).child(id);
+            mDatabaseReference.keepSynced(true);
+        }
+        return mDatabaseReference;
     }
 
     private static final class WeakRefHandler extends Handler {
